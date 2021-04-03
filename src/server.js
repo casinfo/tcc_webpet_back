ebca9106763713c96f369e48fc9f7b3b6da1bf32
CASilva <casilva.com@gmail.com>
2021-04-03 10:45:41 -0300

    configuration port and start prod

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-import app from './app';
+import app from "./app";
 
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
