903c9cbe5a82e3f231960e5d137719f7b86b98c2
CASilva <casilva.com@gmail.com>
2021-04-02 22:42:51 -0300

    added config heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,25 @@
+// module.exports = {
+//     dialect: 'postgres',
+//     host: 'localhost',
+//     username: 'casilva',
+//     password: 'WebPetbdwp01@#',
+//     database: 'bdwp',
+//     define: {
+//       timestamps: true,
+//       underscored: true,
+//       underscoredAll: true,
+//     },
+//   };
+
 module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'casilva',
-    password: 'WebPetbdwp01@#',
-    database: 'bdwp',
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-    },
-  };
-  
+  dialect: "postgres",
+  host: "ec2-54-235-108-217.compute-1.amazonaws.com",
+  username: "sqvawzxtkshqap",
+  password: "4156bc4092d3337f11025cc03f3d989d9c3f42fbf71f8187b98d05edda539696",
+  database: "d26kct279rfopl",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
