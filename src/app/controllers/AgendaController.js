ba646c1f2ac3650321215687e99f789d9b46d346
CASilva <casilva.com@gmail.com>
2021-04-07 20:56:35 -0300

    included route storeConfirma.

--- src/app/controllers/AgendaController.js
+++ src/app/controllers/AgendaController.js
@@ -70,7 +70,7 @@ class AgendaController {
     });
     const agenda = await Agenda.findByPk(id);
 
-    const agendaAtualizada = await agenda.updateConfirma(req.body);
+    const agendaAtualizada = await agenda.update(req.body);
 
     return res.json(agendaAtualizada);
   }
