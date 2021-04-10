a81b9c78534022caa145d8d8ccea43de4d55a18a
CASilva <casilva.com@gmail.com>
2021-04-10 16:02:14 -0300

    included model, controller e routes dashboard.

--- src/database/index.js
+++ src/database/index.js
@@ -1,14 +1,30 @@
-import Sequelize from 'sequelize';
+import Sequelize from "sequelize";
 // Config
-import databaseConfig from '../config/database';
+import databaseConfig from "../config/database";
 // Models
-import Usuario from '../app/models/Usuario';
-import Cliente from '../app/models/Cliente';
-import Pets from '../app/models/Pets';
-import Servico from '../app/models/Servico';
-import Agenda from '../app/models/Agenda';
+import Usuario from "../app/models/Usuario";
+import Cliente from "../app/models/Cliente";
+import Pets from "../app/models/Pets";
+import Servico from "../app/models/Servico";
+import Agenda from "../app/models/Agenda";
+import DashClientes from "../app/models/DashClientes";
+import DashPets from "../app/models/DashPets";
+import DashServicos from "../app/models/DashServicos";
+import DashAgendaMes from "../app/models/DashAgendaMes";
+import DashAgendaSem from "../app/models/DashAgendaSem";
 
-const models = [Usuario, Cliente, Pets, Servico, Agenda];
+const models = [
+  Usuario,
+  Cliente,
+  Pets,
+  Servico,
+  Agenda,
+  DashClientes,
+  DashPets,
+  DashServicos,
+  DashAgendaMes,
+  DashAgendaSem,
+];
 
 class Database {
   constructor() {
@@ -19,11 +35,11 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models.map((model) => model.init(this.connection));
   }
 
   associate() {
-    models.forEach(model => {
+    models.forEach((model) => {
       if (model.associate) {
         model.associate(this.connection.models);
       }
